9d35e0f579d20694112f4c6a74d7d7ec92c6019a
chesterchua <chesterchua722@gmail.com>
2021-03-15 13:49:57 +0800

    Update paper4.c

--- paper4.c
+++ paper4.c
@@ -8,16 +8,16 @@ int main(void) {
   int n;// total number of processes
   int bt[MAX]; //burst time of processes
   int rem_bt[MAX]; // remaining burst time of processes; index 0 is burst time of first process
+  int ready_queue[MAX];
+  int at[MAX]; //arrival time of processes
   int wt[MAX]; //waiting time of each process
   int tat[MAX]; // turnaround time of each process
   int ct[MAX]; // completion time of each process
-  int ps[MAX];// keeps tracks of process sequence
-  int ps_index; //keeps track of index of ps array
-  int bt_max; //max burst time 
+  int bt_max = -1; //max burst time 
   int qt; //quantum time
   int p_counter; //keeps track of number of processes done
-  int time; // keeps track of time
-
+  int time = 0; // keeps track of time
+  int index = -1; //index of next job to run in ready_queue
   
   //initialisation of arrays
   for (int i = 0; i < MAX; i++){
@@ -26,6 +26,8 @@ int main(void) {
     wt[i] = -1;
     tat[i] = -1;
     ct[i] = -1;
+    at[i] = -1;
+    ready_queue[i] = -1;
   }
 
   //Gather user input
@@ -35,90 +37,250 @@ int main(void) {
     printf("Enter Burst Time of process %d: ", i+1);
     scanf("%d", &bt[i]);
     rem_bt[i] = bt[i];
+    printf("Enter Arrival Time of process %d: ", i+1);
+    scanf("%d", &at[i]);
   }
 
 
-  //Find max burst time in the burst times
-  bt_max = -1;
-  for (int i = 0; i < n; i++){
-    if (bt[i] > bt_max){
-      bt_max = bt[i];
+  
+
+  //add process in ready_queue
+  for (int i = 0; i < n; i ++){
+    if (at[i] <= time){
+      ready_queue[i] = rem_bt[i];
     }
   }
 
-  //set quantum time
-
+  //find bt_max in ready queue
+  for (int i = 0; i < n; i++){
+    if (ready_queue[i] > bt_max){
+      bt_max = ready_queue[i];
+    }
+  }
   qt = PROPORTION * bt_max;
-  p_counter = 0;
-  time = 0;
-  //loop through rem_bt; allow burst times smaller than qt to run
+  int k = 0;//keeps track if only element in ready_queue
+  //find index of ready_queue
   for (int i = 0; i < n; i++){
-    if (p_counter == n){
+    if (ready_queue[i] != -1 && ready_queue[i] <= qt){
+      index = i;
       break;
+    } else{
+      k += 1;
     }
-    if (rem_bt[i] <= qt){ //run if burst time less than qt
-      time += rem_bt[i];
-      ct[i] = time;
-      ps[ps_index] = i + 1;
-      rem_bt[i] = -1;
-      p_counter += 1;
-      ps_index += 1;
-    }
-
   }
-  //change quantum time to bt_max; allow the larger burst times to run
-  qt = bt_max;
-  for (int i = 0; i < n; i++){
-    if (p_counter == n){
-      break;
-    } else if (rem_bt[i] != -1){
-      time += rem_bt[i];
-      ct[i] = time;
-      ps[ps_index] = i + 1;
-      rem_bt[i] = -1;
-      p_counter += 1;
-      ps_index += 1;
-    }
-
+  if (k == n){//means that only 1 element in ready_queue
+    for (int i = 0; i < n; i++){
+      if (ready_queue[i] != -1){
+        index = i;
+        break;
+      } 
+    } 
   }
 
+  
+  
+  while (p_counter != n){
+    if (index == -1){//empty ready_queue
+      time += 1;
+      int ready_queue_copy[n];
+      for (int i = 0; i < n; i++){
+        ready_queue_copy[i] = ready_queue[i];
+      }
+      
+      //add process in ready_queue
+      for (int i = 0; i < n; i ++){
+        if (at[i] <= time){
+          ready_queue[i] = rem_bt[i];
+        }
+      }
+      // compare if any new changes to ready_queue
+      for (int i = 0; i < n; i ++){
+        //calculate new qt only when new items added
+        if (ready_queue_copy[i] != ready_queue[i]){
+          for (int i = 0; i < n; i++){
+            if (ready_queue[i] > bt_max){
+              bt_max = ready_queue[i];
+            }
+          }
+          qt = PROPORTION * bt_max;
+        }      
+      }
+      int k = 0;//keeps track if only element in ready_queue
+      //find index of ready_queue
+      for (int i = 0; i < n; i++){
+        if (ready_queue[i] != -1 && ready_queue[i] <= qt){
+          index = i;
+          break;
+        } else{
+          k += 1;
+        }
+      }
+      if (k == n){//means that only 1 element in ready_queue
+        for (int i = 0; i < n; i++){
+          if (ready_queue[i] != -1){
+            index = i;
+            break;
+          } 
+        } 
+      } 
+      //index will remain as -1 if no new items
+      continue;
+    }
+    if (ready_queue[index] <= qt){
+      //run CPU
+      time += ready_queue[index];
+      ready_queue[index] = -1;
+      rem_bt[index] = -1;
+      ct[index] = time;
+      p_counter += 1;
+      int ready_queue_copy[n];
+      for (int i = 0; i < n; i++){
+        ready_queue_copy[i] = ready_queue[i];
+      }
+       //add in new processes
+
+      for (int i = 0; i < n; i ++){
+        if (at[i] <= time){
+          ready_queue[i] = rem_bt[i];
+        }
+      }
+
+      // compare if any new changes to ready_queue
+      for (int i = 0; i < n; i ++){
+        //calculate new qt only when new items added
+        if (ready_queue_copy[i] != ready_queue[i]){
+          for (int i = 0; i < n; i++){
+            if (ready_queue[i] > bt_max){
+              bt_max = ready_queue[i];
+            }
+          }
+          qt = PROPORTION * bt_max;
+        }      
+      }
+      // if qt is -ve, it means ready_queue is empty
+      if (qt < 0){
+        index = -1;
+        continue;
+      }
+      //find a new process that can run in ready_queue and redirect index to point to the burst time in rem_bt
+      for (int i = 0; i < n; i++){
+        if (ready_queue[i] != -1 && ready_queue[i] <= qt){
+          index = i;
+          break;
+        }
+      }
+      //check if remaining processes in ready queue have a higher burst time than qt
+      //if all have higher burst time than qt, set qt = bt_max
+      int check = 0;
+      for (int i = 0; i < n; i++){
+        if (ready_queue[i] != -1 && ready_queue[i] <= qt){
+          check = 1;
+          break;
+        }
+      }
+      if (check == 0){
+        qt = bt_max;
+         //find a new process that can run in ready_queue and redirect index
+        for (int i = 0; i < n; i++){
+          if (ready_queue[i] != -1 && ready_queue[i] <= qt){
+            index = i;
+            break;
+          }
+        }
+      }
+
+    } else{
+      //allow current process to run partially  
+      time += qt;
+      rem_bt[index] -= qt;
+      ready_queue[index] -= qt;
+      int ready_queue_copy[n];
+      for (int i = 0; i < n; i++){
+        ready_queue_copy[i] = ready_queue[i];
+      }
+       //add in new processes
+
+      for (int i = 0; i < n; i ++){
+        if (at[i] <= time){
+          ready_queue[i] = rem_bt[i];
+        }
+      }
+
+      // compare if any new changes to ready_queue
+      for (int i = 0; i < n; i ++){
+        //calculate new qt only when new items added
+        if (ready_queue_copy[i] != ready_queue[i]){
+          for (int i = 0; i < n; i++){
+            if (ready_queue[i] > bt_max){
+              bt_max = ready_queue[i];
+            }
+          }
+          qt = PROPORTION * bt_max;
+        }      
+      }
+      // if qt is -ve, it means ready_queue is empty
+      if (qt < 0){
+        index = -1;
+        continue;
+      }
+      //after this line, ready_queue is not empty
+      //if current process remaining time is  smaller than new qt, allow it to finish running
+      if (ready_queue[index] <= qt){
+        time += ready_queue[index];
+        ready_queue[index] = -1;
+        rem_bt[index] = -1;
+        ct[index] = time;
+        p_counter += 1;      
+      }
+      //find a new process that can run in ready_queue and redirect index
+      for (int i = 0; i < n; i++){
+        if (ready_queue[i] != -1 && ready_queue[i] <= qt){
+          index = i;
+          break;
+        }
+      }
+      //check if remaining processes in ready queue have a higher burst time than qt
+      //if all have higher burst time than qt, set qt = bt_max
+      int check = 0;
+      for (int i = 0; i < n; i++){
+        if (ready_queue[i] != -1 && ready_queue[i] <= qt){
+          check = 1;
+          break;
+        }
+      }
+      if (check == 0){
+        qt = bt_max;
+         //find a new process that can run in ready_queue and redirect index
+        for (int i = 0; i < n; i++){
+          if (ready_queue[i] != -1 && ready_queue[i] <= qt){
+            index = i;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+
+  //calculate turnaround time of each process
+  for (int i = 0; i < n; i++){
+    tat[i] = ct[i] - at[i];
+  }
   //calculate waiting time of each process
-  //completion time equal to turnaround time because assume arrival time equal 0
   for (int i = 0; i < n; i++){
-    wt[i] = ct[i] - bt[i];
+    wt[i] = tat[i] - bt[i];
   }
-
-  //print results
   int total_wt = 0;
   int total_tat = 0;
 
-  printf("\nProcess     Burst Time     Waiting Time     Turnaround Time\n");
+  printf("\nProcess     Arrival Time     Burst Time     Waiting Time     Turnaround Time\n");
   for (int i = 0; i < n; i++){
     total_wt += wt[i];
-    total_tat += ct[i];
-    printf("%5d%10d%20d%20d\n", i+1, bt[i], wt[i], ct[i]);
+    total_tat += tat[i];
+    printf("%5d%10d%20d%20d%20d\n", i+1, at[i],bt[i], wt[i], tat[i]);
   }
-
-  printf("\nProcess Sequence: ");
-  for (int i = 0; i < n; i++){
-    printf("Process %d", ps[i]);
-    if (i == n -1){
-      printf("\n");
-      
-    }else{
-      printf(" --> ");
-    }
-  }
-
-  printf("\nDynamic Time Quantum: %.1f\n", PROPORTION * bt_max);
-  printf("Maximum Burst Time: %d\n", bt_max);
   printf("Average Waiting Time: %.1f\n", total_wt/ (float)n);
   printf("Average Turnaround Time: %.1f\n", total_tat/ (float)n);
-
-
-
-
-
-
+  
   return 0;
 }
